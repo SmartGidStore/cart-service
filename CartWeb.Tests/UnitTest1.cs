51f027cdb98eb45467a53e68eeecd30b0d528224
KolyaKolya95 <kolya.kolya1495@gmail.com>
2017-09-21 13:54:42 +0300

    first commit

---
+++ CartWeb.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CartWeb.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
